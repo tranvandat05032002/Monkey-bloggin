7d771759f9d0dccb587e57b2ef1393df2122a025
Spider Man <tranvandathtml0503@gmail.com>
2022-11-05 01:36:35 +0700

    validation form, this is component using library toastify show error

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -7,23 +7,49 @@ import { Label } from "component/label";
 import React from "react";
 import { useForm } from "react-hook-form";
 import AuthenticationPage from "./AuthenticationPage";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { toast } from "react-toastify";
 // import { useNavigate } from "react-router-dom";
 
 const SignInPage = () => {
+  const validateScheme = yup.object({
+    emailAddress: yup
+      .string()
+      .required("Please enter your email address")
+      .email("Please enter valid email address"),
+    password: yup
+      .string()
+      .required("Please enter your password")
+      .min(8, "Your password must be at least 8 character or greater"),
+  });
+
   const {
     handleSubmit,
     control,
     formState: { isSubmitting, isValid, errors },
   } = useForm({
     mode: "onChange",
+    resolver: yupResolver(validateScheme),
   });
+  React.useEffect(() => {
+    const arrayErrors = Object.values(errors);
+    if (arrayErrors.length > 0) {
+      toast.error(arrayErrors[0]?.message, {
+        pauseOnHover: false,
+        delay: 0,
+      });
+    }
+  }, [errors]);
   //   const { userInfo } = useAuth();
   //   const navigate = useNavigate();
   //   React.useEffect(() => {
   //     !userInfo.email ? navigate("/sign-up") : navigate("/");
   //   }, []);
   //   console.log(userInfo);
-  const handleSignIn = () => {};
+  const handleSignIn = async (values) => {
+    console.log(values);
+  };
   return (
     <div>
       <AuthenticationPage>
