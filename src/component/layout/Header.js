86f69e621786ac8771e6887329d98f98b21e9142
Spider Man <tranvandathtml0503@gmail.com>
2022-11-05 03:47:28 +0700

    Header component

---
+++ src/component/layout/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+const HeaderStyles = styled.div``;
+
+const Header = () => {
+  return (
+    <HeaderStyles>
+      <a href="/">
+        <img srcSet="/logo.png 2x" alt="monkey-blogging" />
+      </a>
+    </HeaderStyles>
+  );
+};
+
+export default Header;
