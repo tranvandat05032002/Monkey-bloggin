3a1117f6496dd09b7f1a06053558c87e33c09bc7
Spider Man <tranvandathtml0503@gmail.com>
2022-11-09 11:02:37 +0700

    UI component PostDetailsPage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import PostDetailsPage from "module/post/PostDetailsPage";
 import HomePage from "pages/HomePage";
 import NotFoundPage from "pages/NotFoundPage";
 import SignInPage from "pages/SignInPage";
@@ -14,6 +15,10 @@ function App() {
           <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
           <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
           <Route path="*" element={<NotFoundPage></NotFoundPage>}></Route>
+          <Route
+            path="/:slug"
+            element={<PostDetailsPage></PostDetailsPage>}
+          ></Route>
         </Routes>
       </AuthProvider>
     </div>
